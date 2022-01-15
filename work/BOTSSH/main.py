4ab2e1d19fe18b29a42b4878e6848c799e0f44f7
Sergey Botsman <serbotsgeyman@gmail.com>
2022-01-15 19:23:24 +0300

    BOTSSH add convert fole utf-8 with bom to utf-8

--- work/BOTSSH/main.py
+++ work/BOTSSH/main.py
@@ -1,71 +1,133 @@
-import paramiko, time, os, getpass
+import threading, paramiko, time, getpass, os
 
-print('--- НАЧАЛО РАБОТЫ ---')
-user = input('ВВЕДИТЕ ЛОГИН: ')
-secret = getpass.getpass('ВВЕДИТЕ ПАРОЛЬ: ')
-port = 22
-short_pause = int(input('ВВЕДИТЕ ВРЕМЯ ОЖИДАНИЯ МЕЖДУ КОММАНДАМИ В СЕКУНДАХ: '))
-
+strdata=''
+fulldata=''
+alldata=''
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
+class ssh:
+    shell = None
+    client = None
+    transport = None
 
-def run_commands():
-    for comand in comands:
-        stdin, stdout, stderr = client.exec_command(comand)
-        
-        print('--- comand:', comand, '---')
-        
-        time.sleep(short_pause)
+    def __init__(self, address, username, password):
+        print("Connecting to server on ip", str(address) + ".")
+        self.client = paramiko.client.SSHClient()
+        self.client.set_missing_host_key_policy(paramiko.client.AutoAddPolicy())
+        self.client.connect(address, username=username, password=password, look_for_keys=False)
+        self.transport = paramiko.Transport((address, 22))
+        self.transport.connect(username=username, password=password)
 
-        # Читаем результат
-        data = stdout.read().decode(encoding='UTF-8') +  stderr.read().decode(encoding='UTF-8')
-        print('---data:',data, '---')
+        thread = threading.Thread(target=self.process)
+        thread.daemon = True
+        thread.start()
 
+    def close_connection(self):
+        if(self.client != None):
+            self.client.close()
+            self.transport.close()
+
+    def open_shell(self):
+        self.shell = self.client.invoke_shell()
+
+    def send_shell(self, command):
+        if(self.shell):
+            self.shell.send(command + "\n")
+        else:
+            print("Shell not opened.")
+
+    def process(self):
+        global strdata, fulldata, alldata
+        while True:
+            # Print data when available
+            if self.shell is not None and self.shell.recv_ready():
+                alldata = self.shell.recv(1024)
+                while self.shell.recv_ready():
+                    alldata += self.shell.recv(1024)                
+                strdata = strdata + str(alldata.decode(encoding='UTF-8'))
+                
+                fulldata = fulldata + str(alldata)
+                strdata = self.print_lines(strdata) # print all received data except last line
+                
+
+    def print_lines(self, data):
+        last_line = data
+        if '\n' in data:
+            lines = data.splitlines()
+            for i in range(0, len(lines)-1):
+                print(lines[i])
+            last_line = lines[len(lines) - 1]
+            if data.endswith('\n'):
+                print(last_line)
+                last_line = ''
+        return last_line
+
+#преобразование кодировки UTF-8 with BOM to UTF-8        
+s = open(os.path.join(current_dir,'list_ip.txt'), mode='r', encoding='utf-8-sig').read()
+open(os.path.join(current_dir,'list_ip.txt'), mode='w', encoding='utf-8').write(s)
+
+s = open(os.path.join(current_dir,'commands.txt'), mode='r', encoding='utf-8-sig').read()
+open(os.path.join(current_dir,'commands.txt'), mode='w', encoding='utf-8').write(s)
 
 #чтение файла ip
-with open(os.path.join(current_dir,'list_ip.txt'), 'r') as list_ip_file:
+with open(os.path.join(current_dir,'list_ip.txt'), 'rb') as list_ip_file:
     list_ip = []
     for line in list_ip_file:
-        line = line.strip()
+        line = line.strip().decode(encoding='UTF-8')
         if line == '':
             continue
         list_ip.append(line)
 
 #чтение файла команд
-with open(os.path.join(current_dir,'comands.txt'),'r') as comands_file:
-            comands = []
-            for line in comands_file:
-                line = line.strip()
+with open(os.path.join(current_dir,'commands.txt'),'rb') as commands_file:
+            commands = []
+            for line in commands_file:
+                line = line.strip().decode(encoding='UTF-8')
                 if line == '':
                     continue
-                comands.append(line)
+                commands.append(line)
 
-for ip in list_ip: 
-    host = ip
-    print('---', ip, '---')
 
+
+print('--- НАЧАЛО РАБОТЫ ---')
+sshServer = "10.3.136.141"
+sshUsername = input('ВВЕДИТЕ ЛОГИН: ')
+sshPassword = getpass.getpass('ВВЕДИТЕ ПАРОЛЬ: ')
+short_pause = int(input('ВВЕДИТЕ ВРЕМЯ ОЖИДАНИЯ МЕЖДУ КОММАНДАМИ В СЕКУНДАХ: '))
+
+
+
+for sshServer in list_ip: 
+    print('---', sshServer, '---')
     try:
-        client = paramiko.SSHClient()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        # Подключение
-        
-        run_commands()
-
-            
-        client.close()
+        connection = ssh(sshServer, sshUsername, sshPassword)
+        connection.open_shell()
+        for command in commands:
+            print('---', command)
+            connection.send_shell(command)      
+            time.sleep(short_pause)
+                  
+        connection.close_connection()
         
         with open(os.path.join(current_dir,'list_ip_good.txt'), 'a') as list_ip_good_file:
-            list_ip_good_file.write(str(ip)+'\n')
+            list_ip_good_file.write(str(sshServer)+'\n')
 
 
     except TimeoutError:
         print('не получен нужный отклик, или было разорвано уже установленное соединение')
         with open(os.path.join(current_dir,'list_ip_bad.txt'), 'a') as list_ip_good_bad:
-            list_ip_good_bad.write(str(ip)+ '\n')
+            list_ip_good_bad.write(str(sshServer)+ '\n')
     except paramiko.AuthenticationException:
         print('Authentication failed')
         with open(os.path.join(current_dir,'list_ip_bad.txt'), 'a') as list_ip_good_bad:
-            list_ip_good_bad.write(str(ip)+ '\n')
-    
+            list_ip_good_bad.write(str(sshServer)+ '\n')
+    finally: 
+        try:
+            connection.close_connection()
+            
+        except NameError:
+            pass
+        finally: print('---', sshServer, 'close---')
 
+            
 print('\n  --- КОНЕЦ ВСЕХ ОПЕРАЦИЙ ---\n')
--- work/BOTSSH/test/test.py
+++ work/BOTSSH/test/test.py
@@ -3,12 +3,16 @@ import os
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-with open(os.path.join(current_dir,'commands.txt'),'rb') as commands_file:
-            commands = []
-            for line in commands_file:
-                line = line.strip().decode(encoding='UTF-8')
-                if line == '':
-                    continue
-                commands.append(line)
-print(commands)
-            
+# with open(os.path.join(current_dir,'commands.txt'),'rb') as commands_file:
+#             commands = []
+#             for line in commands_file:
+#                 line = line.strip().decode(encoding='UTF-8')
+#                 if line == '':
+#                     continue
+#                 commands.append(line)
+# print(commands)
+
+s = open(os.path.join(current_dir,'UTF8withBOM.txt'), mode='r', encoding='utf-8-sig').read()
+open(os.path.join(current_dir,'UTF8withBOM.txt'), mode='w', encoding='utf-8').write(s)
+    
+        
